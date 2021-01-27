227e4ed43da3e130d0b3004f83cdda64b2be81dc
newilgreen <77456413+newilgreen@users.noreply.github.com>
2021-01-27 17:55:57 +0300

    Изменен способ формирования файла error.json
    Изменен способ формирования файла сообщающего об ошибке.
    Добавлены комментарии

--- avito_autotest_dokshin.py
+++ avito_autotest_dokshin.py
@@ -1,16 +1,21 @@
 import json
+# json потребуется для манипуляций с входными и выходными фаилами
 import sys 
+# sys потребуется для прекращения работы программы после обнаружения ошибки
 
 def new_test(ts):
+    # функция new_test(ts) основная часть программы
+    # она заполняет TestcaseStructure.json згначениями из Values.json
+    # ts - это декодированый TestcaseStructure.json из формата json в dist
+    first_chek_p(ts)
     for params in ts:
         params = ts['params']
-        value_chek(params)
         for i in range(len(params)):
-            one(params[i])
-            onep(params[i])
+            onei(params[i], 'TestcaseStructure.json')
+            onev_ts(params[i])
+            onet(params[i], 'TestcaseStructure.json')
             if 'values' in params[i]:
                 values1 = params[i]['values'] 
-                value_chek(values1)
                 for z in range(len(values1)):
                     if 'params' in values1[z]:
                         new_test(values1[z])
@@ -21,6 +26,10 @@ def new_test(ts):
                 easy_case(vs,params,i)
 
 def easy_case(vs, params, i):
+    # функция easy_case(vs, params, i) записывает значения value, если нет values
+    # vs - это декодированый Values.json из формата json в dist
+    # params - это основной элемент из TestcaseStructure.json
+    # i - номер основного элемента  
     for values2 in vs:
         values2 = vs['values']
         flag = 0
@@ -35,13 +44,17 @@ def easy_case(vs, params, i):
                 break
 
 def hard_case(vs, params, i, z):
+    # функция hard_case(vs, params, i, z) записывает значения value, если есть values
+    # vs - это декодированый Values.json из формата json в dist
+    # params - это основной элемент из TestcaseStructure.json
+    # i - номер основного элемента(params) 
+    # z - номер элемента values
     for values2 in vs:
         values2 = vs['values']
         flag = 0
         for j in range(len(values2)):
             for id in values2[j]:
                 if values2[j]['value'] == params[i]['values'][z]['id']:
-                    value_value(params[i]['value'])
                     params[i]['value'] = params[i]['values'][z]['title']
                     flag = 1
                     break
@@ -49,90 +62,147 @@ def hard_case(vs, params, i, z):
                 break
 
 def validate(filename, error_str):
+    # функция validate(filename, error_str) проводит проверку на возможность декодировать входной фаил
+    # filename - входной фаил
+    # error_str - словарь показывающий информацию об ошибке
     with open(filename) as file:
         try:
             return json.load(file) 
-        except:
-            print_error_file(error_str)  
+        except :
+            print_error_file(error_str)
+
 
 def print_error_file(error_str):
+    # функция print_error_file(error_str) выводит фаил error.json, сообщающий об ошибке
+    # error_str - словарь показывающий информацию об ошибке
     error_file = open('error.json', mode='w', encoding="utf-8")
     error_file.write(json.dumps(error_str, indent=2, ensure_ascii=False))
     sys.exit()
 
+
 def print_new_file(dist):
+    # функуция print_new_file(dist) записывает фаил StructureWithValues.json
+    # dist - словарь который требуется записать в фаил 
     new_file = open('StructureWithValues.json', mode='w', encoding="utf-8")
     new_file.write(json.dumps(dist, indent=2, ensure_ascii=False))
 
-def one(dist):
-    if 'id' not in dist or 'value' not in dist:
-        print_error_file(error_str2)
-    else:
-        value_chek(dist['id'])
 
-def onep(dist):
-    if 'title' not in dist:
-        print_error_file(error_str2)
+def onei(dist, location):
+    # функуция onei(dist) проверяет наличие ключевого параметра id
+    # dist - словарь который требуется проверить 
+    if 'id' not in dist:
+        print_error_file(get_error(error_base, location, 1, 'id'))
     else:
-        value_chek(dist['title'])
+        value_chek(dist['id'], "id", location)
+
+
+def onev_ts(dist):
+    # функуция onev(dist) проверяет наличие ключевого параметра value 
+    # dist - словарь часть TestcaseStructure.json
+    if 'value' not in dist:
+        print_error_file(get_error(error_base, 'TestcaseStructure.json', 1, 'value'))
+
+
+def onev_vs(dist):
+    # функуция onev(dist) проверяет наличие ключевого параметра value 
+    # dist - словарь часть Values.json
+    if 'value' not in dist:
+        print_error_file(get_error(error_base, 'Values.json', 1, 'value'))
+    else:
+        value_chek(dist['value'], 'value', 'Values.json')
+
+
+def onet(dist, location):
+    # функуция onet(dist) проверяет наличие ключевого параметра title 
+    # dist - словарь который требуется проверить 
+    if 'title' not in dist:
+        print_error_file(get_error(error_base, 'TestcaseStructure.json', 1, 'title'))
+    else:
+        value_chek(dist['title'], 'title', location)
+
+
+def first_chek_p(dist):
+    # функуция first_chek_p(dist) проверяет наличие ключевого параметра 'params'
+    # dist - 'TestcaseStructure.json' 
+    if 'params' not in dist:
+        print_error_file(get_error(error_base, 'TestcaseStructure.json', 1, 'params'))
+
+
+def first_chek_v(dist):
+    # функуция first_chek_v(dist) проверяет наличие ключевого параметра 'values'
+    # dist - 'Values.json' 
+    if 'values' not in dist:
+        print_error_file(get_error(error_base, 'Values.json', 1, 'values'))
+    else:
+        for j in range(len(dist['values'])):
+            onei(dist['values'][j], 'Values.json')
+            onev_vs(dist['values'][j])
+
 
 def value_value(value):
+    # функуция value_value(value) проверяет пусты ли значение value в TestcaseStructure.json
+    # value - проверяемое значение
     if value != "":
-        print_error_file(error_str3)
+        print_error_file(get_error(error_base, 'TestcaseStructure.json', 2, 'value is not null'))
 
-def value_chek(value):
+
+def value_chek(value, spf, location):
+    # функуция value_chek(value) проверяет не пуст ли ключевой параметр(id,title,values,params)
+    # value - проверяемое значение
     if value == "":
-        print_error_file(error_str4)
+        print_error_file(get_error(error_base, location, 3, spf))
 
-error_str1 = """
-{
-    "error": {
-        "message": "Входные файлы некорректны, невозможно декодировать один из фаилов"
-    }
-}"""
-error_str1 = json.loads(error_str1)
 
-error_str2 = """
-{
-    "error": {
-        "message": "Входные файлы некорректны, ключевой параметр не обнаружен"
-    }
-}"""
-error_str2 = json.loads(error_str2)
+def get_error(error_base, location, i, spf):
+    # функция get_error(error_base, location, i, spf) формерует сообщение об ошибке
+    # на входе незаполенный словарь error_base
+    # location = location
+    # i = id
+    # spf = specifically
+    # на выходе заполенный словарь error_base в соотвествии с ошибкой 
+    # в котором:
+    #   id - идентификатор ошибки
+    #   location - фаил в котором обнаружена ошибка
+    #   title - тип ошибки
+    #   specifically - уточнение
+    #   message - сообщение об ошибке  
+    error_base["error"]["id"] = i
+    error_base["error"]["location"] = location
+    if i == 0:
+        error_base["error"]["title"] = "file defect"
+        error_base["error"]["specifically"] = spf
+        error_base["error"]["message"] = "Невозможно декодировать один из фаилов"
+    elif i == 1:
+        error_base["error"]["title"] = "value defect"
+        error_base["error"]["specifically"] = spf
+        error_base["error"]["message"] = "Ключевой параметр не обнаружен"
+    elif i == 2:
+        error_base["error"]["title"] = "value defect"
+        error_base["error"]["specifically"] = spf
+        error_base["error"]["message"] = "Поля value в TestcaseStructure должны быть пустыми"
+    elif i == 3:
+        error_base["error"]["title"] = "value defect"
+        error_base["error"]["specifically"] = spf
+        error_base["error"]["message"] = "Поле ключевого параметра является пустым"
+    else:
+        error_base["error"]["title"] = "???"
+        error_base["error"]["specifically"] = "???"
+        error_base["error"]["message"] = "???"
 
-error_str3 = """
-{
-    "error": {
-        "message": "Входные файлы некорректны, поля value в TestcaseStructure должны быть пустыми "
-    }
-}"""
-error_str3 = json.loads(error_str3)
+    return error_base
 
-error_str4 = """
-{
-    "error": {
-        "message": "Входные файлы некорректны, поля ключевого параметра является пустым"
-    }
-}"""
-error_str4 = json.loads(error_str4)
 
-error_str5 = """
-{
-    "error": {
-        "message": "Входной файл не найден"
-    }
-}"""
-error_str5 = json.loads(error_str5)
+error_base = {"error" : {"id" : "", "title" : "", "specifically" : "", "location" : "", "message" : ""}}
 
-validate('TestcaseStructure.json', error_str1)
-validate('Values.json', error_str1)
+validate('TestcaseStructure.json', get_error(error_base, 'TestcaseStructure.json', 0, 'file'))
+validate('Values.json', get_error(error_base, 'Values.json', 0, 'file'))
 
 ts = json.loads(open('TestcaseStructure.json' ,mode='r', encoding="utf-8").read())
 vs = json.loads(open('Values.json' ,mode='r', encoding="utf-8").read())
 
-value_chek(vs['values'])
-for j in range(len(vs['values'])):
-    one(vs['values'][j])
+first_chek_v(vs)
 
-new_test(ts) 
+new_test(ts)
 print_new_file(ts)
+# dist = ts на входе это TestcaseStructure.json 
+# с заполеными значениями Vaules из Values.json
